621e1d9ee4dbf8e1ee229a12619df696c0d1ab0d
shupecode <131179846+shupecode@users.noreply.github.com>
2023-10-06 16:26:14 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編 <課題></title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $dates = ['name'=>'onion','price'=> 200,'weitht'=> 160];
+         print_r($dates);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
